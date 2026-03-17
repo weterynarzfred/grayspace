56b4999e0d8633f17fac68ca0da590d62a3fc6a4
weterynarzfred <weterynarzfred@gmail.com>
2026-03-17 16:53:46 +0100

    styling

--- src/components/PanelTypeSwitcher.jsx
+++ src/components/PanelTypeSwitcher.jsx
@@ -3,29 +3,29 @@ import { PANEL_TYPES } from "./panelTypes";
 import styles from "./PanelTypeSwitcher.module.scss";
 
 function PanelTypeSwitcher({ panelType, onPanelTypeChange }) {
-  return (
-    <Select.Root value={panelType} onValueChange={onPanelTypeChange}>
-      <Select.Trigger className={styles.trigger} aria-label="Panel type switcher">
-        <Select.Value />
-        <Select.Icon className={styles.icon}>v</Select.Icon>
-      </Select.Trigger>
-      <Select.Portal>
-        <Select.Content className={styles.content} position="popper" sideOffset={6}>
-          <Select.Viewport className={styles.viewport}>
-            {PANEL_TYPES.map((panelTypeOption) => (
-              <Select.Item
-                key={panelTypeOption.value}
-                className={styles.item}
-                value={panelTypeOption.value}
-              >
-                <Select.ItemText>{panelTypeOption.label}</Select.ItemText>
-              </Select.Item>
-            ))}
-          </Select.Viewport>
-        </Select.Content>
-      </Select.Portal>
-    </Select.Root>
-  );
+  return <Select.Root value={panelType} onValueChange={onPanelTypeChange}>
+    <Select.Trigger className={styles.trigger} aria-label="Panel type switcher">
+      <Select.Value />
+      <Select.Icon className={styles.icon}>
+        <svg viewBox="0 0 10 10">
+          <path d="M1 3L5 7L9 3" />
+        </svg>
+      </Select.Icon>
+    </Select.Trigger>
+    <Select.Portal>
+      <Select.Content className={styles.content} collisionPadding={0} position="popper">
+        <Select.Viewport className={styles.viewport} >
+          {PANEL_TYPES.map(panelTypeOption => <Select.Item
+            key={panelTypeOption.value}
+            className={styles.item}
+            value={panelTypeOption.value}
+          >
+            <Select.ItemText>{panelTypeOption.label}</Select.ItemText>
+          </Select.Item>)}
+        </Select.Viewport>
+      </Select.Content>
+    </Select.Portal>
+  </Select.Root>;
 }
 
 export default PanelTypeSwitcher;
